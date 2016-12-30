b76b2303d51f242fb19e10c4fb3f2f9b30de9bec
UAustin <liuyinggang08@163.com>
2016-12-30 22:31:34 +0800

    initail update

---
+++ diandudu.js
@@ -0,0 +1,375 @@
+var showImg0 = function(imgButton) {
+    var getImg = e('#id-0')
+    bindEvent(getImg, 'click', function(){
+          insertImg(getImg.src)
+           var className = 'hinge'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg1 = function(imgButton) {
+    var getImg = e('#id-1')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceIn'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg2 = function(imgButton) {
+    var getImg = e('#id-2')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceInDown'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+var showImg3 = function(imgButton) {
+    var getImg = e('#id-3')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceInLeft'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg4 = function(imgButton) {
+    var getImg = e('#id-4')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceInRight'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg5 = function(imgButton) {
+    var getImg = e('#id-5')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceInUp'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg6 = function(imgButton) {
+    var getImg = e('#id-6')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceOut'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg7 = function(imgButton) {
+    var getImg = e('#id-7')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceOutDown'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg8 = function(imgButton) {
+    var getImg = e('#id-8')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceOutLeft'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg9 = function(imgButton) {
+    var getImg = e('#id-9')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceOutRight'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg10 = function(imgButton) {
+    var getImg = e('#id-10')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'bounceOutUp'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg11 = function(imgButton) {
+    var getImg = e('#id-11')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeIn'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg12 = function(imgButton) {
+    var getImg = e('#id-12')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'zoomInLeft'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg13 = function(imgButton) {
+    var getImg = e('#id-13')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'zoomIn'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg14 = function(imgButton) {
+    var getImg = e('#id-14')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInLeft'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg15 = function(imgButton) {
+    var getImg = e('#id-15')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInLeftBig'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg16 = function(imgButton) {
+    var getImg = e('#id-16')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInRight'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg17 = function(imgButton) {
+    var getImg = e('#id-17')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInRightBig'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg18 = function(imgButton) {
+    var getImg = e('#id-18')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInUp'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg19 = function(imgButton) {
+    var getImg = e('#id-19')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeInUpBig'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg20 = function(imgButton) {
+    var getImg = e('#id-20')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeOut'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg21 = function(imgButton) {
+    var getImg = e('#id-21')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeOutDown'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg22 = function(imgButton) {
+    var getImg = e('#id-22')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeOutDownBig'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg23 = function(imgButton) {
+    var getImg = e('#id-23')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeOutLeft'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg24 = function(imgButton) {
+    var getImg = e('#id-24')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'fadeOutLeftBig'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showImg25 = function(imgButton) {
+    var getImg = e('#id-25')
+    bindEvent(getImg, 'click', function(){
+           insertImg(getImg.src)
+           var className = 'rollIn'
+           var img = e('.show-big-img')
+           log('img', img)
+           img.classList.add(className)
+        }
+    )
+}
+
+var showDetail = function(photo) {
+    var t = `
+        <img class="show-big-img animated infinite" src="${photo}">
+    `
+    return t
+}
+
+var stopButton = function() {
+    var stopB = e('.stop-button')
+    bindEvent(stopB, "click", function(){
+        var del = e('.show-big-img')
+        var className = "animated"
+        deleteClass(del, className)
+    })
+}
+
+var startButton = function() {
+    var startB = e('.start-button')
+    bindEvent(startB, "click", function(){
+        var add = e('.show-big-img')
+        var className = "animated"
+        addClass(add, className)
+    })
+}
+
+var insertImg = function(photo) {
+    var showDiv = e('.show-oneimg')
+    showDiv.innerHTML = ''
+    t = showDetail(photo)
+    appendHtml(showDiv, t)
+}
+
+var __main = function() {
+    showImg0()
+    showImg1()
+    showImg2()
+    showImg3()
+    showImg4()
+    showImg5()
+    showImg6()
+    showImg7()
+    showImg8()
+    showImg9()
+    showImg10()
+    showImg11()
+    showImg12()
+    showImg13()
+    showImg14()
+    showImg15()
+    showImg16()
+    showImg17()
+    showImg18()
+    showImg19()
+    showImg20()
+    showImg21()
+    showImg22()
+    showImg23()
+    showImg24()
+    showImg25()
+    stopButton()
+    startButton()
+}
+
+__main()
---
+++ dom.js
@@ -0,0 +1,53 @@
+var log = function() {
+    console.log.apply(console, arguments);
+}
+var e = function(selector) {
+    return document.querySelector(selector)
+}
+
+var es = function(selector) {
+    return document.querySelectorAll(selector)
+}
+
+var appendHtml = function(element, html) {
+    element.insertAdjacentHTML('beforeend', html)
+}
+
+var bindEvent = function(element, event, callback) {
+    element.addEventListener(event, callback)
+}
+var bindEventAll = function(selector, event, callback) {
+    var arr = es(selector)
+    for (var i = 0; i < arr.length; i++) {
+        var e = arr[i]
+        bindEvent(e, event, callback)
+    }
+}
+var containsClass = function(element, className) {
+    return element.classList.contains(className)
+}
+
+var addClass = function(element, className) {
+    element.classList.add(className)
+}
+
+var deleteClass = function(element, className) {
+    element.classList.remove(className)
+}
+
+var toggleClass = function(element, className) {
+    if (element != null) {
+        if (containsClass(element, className)) {
+            deleteClass(element, className)
+        } else {
+            addClass(element, className)
+        }
+    }
+}
+
+var removeClassAll = function(elements, className) {
+    for (var i = 0; i < elements.length; i++) {
+        var e = elements[i]
+        deleteClass(e, className)
+    }
+}
